95cbe1877e37ead7c6496f10b4d81b573552d176
lfDev28 <lee.forster28@gmail.com>
2025-11-07 13:10:26 +1100

    Repo reset

--- scripts/install-operator-from-catalog.sh
+++ scripts/install-operator-from-catalog.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-CATALOG_IMG=${CATALOG_IMG:-quay.io/lee_forster/mas-iam-operator:catalog-0.0.7}
+CATALOG_IMG=${CATALOG_IMG:-quay.io/lee_forster/mas-iam-operator:catalog-0.0.8}
 CATALOG_NAME=${CATALOG_NAME:-mas-iam-operator-dev}
 CATALOG_NAMESPACE=${CATALOG_NAMESPACE:-openshift-marketplace}
 CATALOG_DISPLAY_NAME=${CATALOG_DISPLAY_NAME:-"MAS IAM Operator (dev)"}
